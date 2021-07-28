5351068401ae57c86a9e4eab711401a54c28cc7a
lowkeyway <lowkeyway@126.com>
2021-07-28 17:47:45 +0800

    Add node for platform device

--- ovt_touch/ovt_core.c
+++ ovt_touch/ovt_core.c
@@ -1,11 +1,21 @@
 #include "ovt_core.h"
 
 static bool plt_reg = 0;
+static bool node_reg = 0;
+
+extern const struct attribute_group ovt_attr_group;
 
 static int ovt_core_probe(struct platform_device *pdev)
 {
+  int ret = 0;
   printk("%s\n", __func__);
-  return 0;
+  ret = sysfs_create_group(&pdev->dev.kobj, &ovt_attr_group);
+  if(ret){
+    printk("sysfs_create_group fail! ret = %d\n", ret);
+    return ret;
+  }
+  node_reg = 1;
+  return ret;
 }
 
 static int ovt_core_remove(struct platform_device *pdev)
@@ -40,6 +50,8 @@ static void __exit ovt_core_exit(void)
   printk("%s\n", __func__);
   if(plt_reg)
     platform_driver_unregister(&ovt_platform_driver);
+  if(node_reg)
+    sysfs_remove_group(&pdev->dev.kobj, &ovt_attr_group);
   return 0;
 }
 
